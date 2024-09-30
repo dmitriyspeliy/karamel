2a0f13c930a0c163bddf98ffec81b37e47e9c643
d <dmitriypospelov93@gmail.com>
2024-09-30 17:52:09 +0700

    Payment flow + refactoring + change db

--- src/main/java/effective_mobile/com/repository/DealRepository.java
+++ src/main/java/effective_mobile/com/repository/DealRepository.java
@@ -1,10 +1,17 @@
 package effective_mobile.com.repository;
 
 import effective_mobile.com.model.entity.Deal;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface DealRepository extends CrudRepository<Deal, Long> {
     Optional<Deal> findByExtDealId(String extId);
+
+    @Query(nativeQuery = true, value =
+            "select * from deal inner join contact on deal.contact_id = contact.id where ext_contact_id = ?1 and event_id = ?2")
+    Optional<Deal> findDealByContactIdAdnEventId(String contactId, Long eventId);
+
+
 }
--- src/main/java/effective_mobile/com/service/ContactService.java
+++ src/main/java/effective_mobile/com/service/ContactService.java
@@ -1,16 +1,14 @@
 package effective_mobile.com.service;
 
 import effective_mobile.com.model.dto.rq.UpdateContactRequestBody;
-import effective_mobile.com.model.entity.Contact;
 import effective_mobile.com.model.entity.Deal;
-import effective_mobile.com.repository.ContactRepository;
+import effective_mobile.com.repository.DealRepository;
 import effective_mobile.com.service.api.deal.UpdateDealComment;
 import effective_mobile.com.utils.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,23 +17,17 @@ import java.util.Optional;
 public class ContactService {
 
     private final UpdateDealComment updateContact;
-    private final ContactRepository contactRepository;
+    private final DealRepository dealRepository;
 
 
     public void updateEmail(Long id, UpdateContactRequestBody contactRequestBody) throws BadRequestException {
-        Optional<Contact> optionalContact = contactRepository.findByExtContactId(String.valueOf(id));
-        if (optionalContact.isPresent()) {
-            List<Deal> deal = optionalContact.get().getDeal();
-            for (Deal deal1 : deal) {
-                if (deal1.getEvent().getId().equals(contactRequestBody.getLeadId())) {
-                    String extId = deal1.getExtDealId();
-                    updateContact.refreshCommentDeal(
-                            extId,
-                            "Билеты отправлены на почту " + contactRequestBody.getEmail()
-                    );
-                    break;
-                }
-            }
+        Optional<Deal> dealOptional = dealRepository.findDealByContactIdAdnEventId(String.valueOf(id), contactRequestBody.getLeadId());
+        if (dealOptional.isPresent()) {
+            String extId = dealOptional.get().getExtDealId();
+            updateContact.refreshCommentDeal(
+                    extId,
+                    "Билеты отправлены на почту " + contactRequestBody.getEmail()
+            );
         } else {
             throw new BadRequestException("No contact by ext id " + id);
         }

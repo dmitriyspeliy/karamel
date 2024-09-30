63a116e0546db6994616cf5de6568ed5f06b162e
d <dmitriypospelov93@gmail.com>
2024-09-30 18:16:56 +0700

    Payment flow + refactoring + change db

--- src/main/java/effective_mobile/com/repository/DealRepository.java
+++ src/main/java/effective_mobile/com/repository/DealRepository.java
@@ -3,6 +3,7 @@ package effective_mobile.com.repository;
 import effective_mobile.com.model.entity.Deal;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,8 +11,9 @@ public interface DealRepository extends CrudRepository<Deal, Long> {
     Optional<Deal> findByExtDealId(String extId);
 
     @Query(nativeQuery = true, value =
-            "select * from deal inner join contact on deal.contact_id = contact.id where ext_contact_id = ?1 and event_id = ?2 order by create_date desc limit 1")
-    Optional<Deal> findDealByContactIdAdnEventId(String contactId, Long eventId);
+            "select * from deal inner join contact on deal.contact_id = contact.id where ext_contact_id = :contactId and event_id = :eventId order by create_date desc limit 1")
+    Optional<Deal> findDealByContactIdAdnEventId(@Param(value = "contactId") String contactId,
+                                                 @Param(value = "eventId") Long eventId);
 
 
 }

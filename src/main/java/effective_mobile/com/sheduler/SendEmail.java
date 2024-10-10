1cd5bcd6b742db057a13826e1466a24f4f6844b3
d <dmitriypospelov93@gmail.com>
2024-10-10 17:43:40 +0700

    Add payment service

--- src/main/java/effective_mobile/com/sheduler/SendEmail.java
+++ src/main/java/effective_mobile/com/sheduler/SendEmail.java
@@ -3,10 +3,8 @@ package effective_mobile.com.sheduler;
 import effective_mobile.com.model.entity.Contact;
 import effective_mobile.com.model.entity.Deal;
 import effective_mobile.com.model.entity.Invoice;
-import effective_mobile.com.repository.DealRepository;
 import effective_mobile.com.repository.InvoiceRepository;
 import effective_mobile.com.service.EmailService;
-import effective_mobile.com.service.api.payment.GetInvoice;
 import effective_mobile.com.utils.enums.Status;
 import effective_mobile.com.utils.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
@@ -45,7 +43,7 @@ public class SendEmail {
                     "Письмо с Карамельной Фабрики Деда Мороза и ваш билет",
                     emailService.createMessage(deal));
             log.info("Билеты были отправлены на почту " + contact.getEmail());
-            byStatus.setCountOfSendTicket(byStatus.getCountOfSendTicket() + 1);
+            byStatus.setCountOfSendTicket(byStatus.getCountOfSendTicket() == null ? 1 : byStatus.getCountOfSendTicket() + 1);
             invoiceRepository.save(byStatus);
         }
         log.info("Scheduler was finish");

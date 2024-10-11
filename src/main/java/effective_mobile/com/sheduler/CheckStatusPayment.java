ca04bd37ccae7a08bbbe02d624f64bb352022d1f
d <dmitriypospelov93@gmail.com>
2024-10-11 14:40:50 +0700

    Add payment service

--- src/main/java/effective_mobile/com/sheduler/CheckStatusPayment.java
+++ src/main/java/effective_mobile/com/sheduler/CheckStatusPayment.java
@@ -2,10 +2,13 @@ package effective_mobile.com.sheduler;
 
 import effective_mobile.com.model.dto.rs.PaymentResult;
 import effective_mobile.com.model.entity.Deal;
+import effective_mobile.com.model.entity.Event;
 import effective_mobile.com.model.entity.Invoice;
 import effective_mobile.com.repository.DealRepository;
 import effective_mobile.com.repository.InvoiceRepository;
+import effective_mobile.com.service.api.deal.DeleteDeal;
 import effective_mobile.com.service.api.deal.UpdateDealStatus;
+import effective_mobile.com.service.api.event.ChangeEventInBitrix;
 import effective_mobile.com.service.api.payment.GetInvoice;
 import effective_mobile.com.utils.enums.Status;
 import effective_mobile.com.utils.exception.BadRequestException;
@@ -33,12 +36,13 @@ public class CheckStatusPayment {
     private final DealRepository dealRepository;
     private final GetInvoice getInvoice;
     private final UpdateDealStatus updateDealStatus;
+    private final ChangeEventInBitrix changeEventInBitrix;
 
     /**
      * Синхронизируем статус платежа в бд с робокассой
      */
     @Scheduled(fixedRate = 200000L)
-    public void check() throws BadRequestException {
+    public synchronized void check() throws BadRequestException {
         log.info("Start working scheduler. Check invoice's with status PENDING");
         // выгружаем все счета со статусом PENDING
         List<Invoice> invoiceByStatus = invoiceRepository.getInvoiceByStatus(Status.PENDING.name());
@@ -66,17 +70,25 @@ public class CheckStatusPayment {
 
 
     /**
-     * Все платежи, со статусом PENDING, которые уже 2 дня находятся в этом статусе, будут отменены
+     * Все платежи, со статусом PENDING, которые уже 15 минут находятся в этом статусе, будут отменены
      */
-    @Scheduled(fixedRate = 500000L)
-    public void checkInvoiceStatusAndTime() {
+    @Scheduled(fixedRate = 300000L)
+    public synchronized void checkInvoiceStatusAndTime() throws BadRequestException {
         log.info("Start working scheduler. Set FAILURE status in invoice");
         // выгружаем все счета со статусом PENDING
         List<Invoice> invoiceByStatus = invoiceRepository.getInvoiceByStatus(Status.PENDING.name());
         for (Invoice byStatus : invoiceByStatus) {
-            if (byStatus.getCreateAt().plusDays(2).isBefore(LocalDateTime.now())) {
+            Deal deal = byStatus.getDeal();
+            Event event = byStatus.getDeal().getEvent();
+            if (byStatus.getCreateAt().plusMinutes(15).isBefore(LocalDateTime.now())) {
                 byStatus.setStatus(Status.FAILURE);
                 invoiceRepository.save(byStatus);
+                if (event.getType().contains("ШКОЛЬНЫЕ")) {
+                    changeEventInBitrix.undoChangingInSchoolEvent(event);
+                } else if (event.getType().contains("СБОРНЫЕ")) {
+                    changeEventInBitrix.undoChangingInMixedEvent(deal.getKidCount(),
+                            deal.getAdultCount(), event);
+                }
             }
         }
         log.info("Scheduler was finish");

e0e6ca347a3aa390a4e609ff8348ce7638fc6813
d <dmitriypospelov93@gmail.com>
2024-10-17 13:13:53 +0700

    Samara

--- src/main/java/ru/kotomore/excursionapi/services/TokenServiceUseCase.java
+++
@@ -1,9 +0,0 @@
-package ru.kotomore.excursionapi.services;
-
-import ru.kotomore.excursionapi.models.TokenResponse;
-
-public interface TokenServiceUseCase {
-    TokenResponse getTokenResponse();
-
-    TokenResponse refreshToken(TokenResponse tokenResponse);
-}

97bb47703c726155bab56cca04b53621be441327
Dmitriy Pospelov <dmitriypospelov93@gmail.com>
2024-10-26 18:10:27 +0700

    ADD prometheus

--- src/main/java/effective_mobile/com/controller/CityInfoController.java
+++ src/main/java/effective_mobile/com/controller/CityInfoController.java
@@ -3,6 +3,7 @@ package effective_mobile.com.controller;
 import effective_mobile.com.configuration.properties.CityProperties;
 import effective_mobile.com.model.dto.rs.CityInfoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@Slf4j
 public class CityInfoController {
 
     private final CityProperties cityProperties;
@@ -20,6 +22,7 @@ public class CityInfoController {
      */
     @GetMapping("/city")
     public CityInfoResponse getCityInfo(@RequestParam(name = "city") String city) {
+        log.info("Get city info by city " + city);
         CityProperties.Info info = cityProperties.getCityInfo().get(city);
         return new CityInfoResponse(
                 info.getCityName(),

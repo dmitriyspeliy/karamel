4201b431bc2e9fce2c5d2fbb997b7b7c41058b47
Dmitriy Pospelov <dmitriypospelov93@gmail.com>
2024-11-23 13:31:56 +0700

    Update count of sms fix problem

--- src/main/java/effective_mobile/com/repository/InvoiceRepository.java
+++ src/main/java/effective_mobile/com/repository/InvoiceRepository.java
@@ -3,8 +3,10 @@ package effective_mobile.com.repository;
 import effective_mobile.com.model.entity.Invoice;
 import effective_mobile.com.utils.enums.Status;
 import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,5 +34,10 @@ public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
     @EntityGraph(value = "invoice_deal_event")
     @Query("select inv from Invoice inv where inv.status = ?1 and inv.createAt > ?2 and inv.countOfSendSms = 0")
     List<Invoice> getDealAndEventAndContactByStatusSms(Status status, LocalDateTime localDateTime);
+
+    @Modifying
+    @Transactional
+    @Query("update Invoice inv set inv.countOfSendSms = 1 where inv.dealId = ?1")
+    int updateCountOfSmsByDealId(Long dealId);
 
 }
--- src/main/java/effective_mobile/com/sheduler/SmsSender.java
+++ src/main/java/effective_mobile/com/sheduler/SmsSender.java
@@ -3,6 +3,7 @@ package effective_mobile.com.sheduler;
 import effective_mobile.com.model.entity.Contact;
 import effective_mobile.com.model.entity.Deal;
 import effective_mobile.com.model.entity.Invoice;
+import effective_mobile.com.model.entity.SmsInfo;
 import effective_mobile.com.repository.InvoiceRepository;
 import effective_mobile.com.service.SmsService;
 import effective_mobile.com.utils.enums.CityInfo;
@@ -38,8 +39,12 @@ public class SmsSender {
             String sms = "Предоплата за экскурсию получена. Дальнейшие действия тут " + linkToSmsAction;
             // отправляем sms
             smsService.sendSms(deal.getExtDealId(), contact.getPhone(), sms);
-            byStatus.setCountOfSendSms(1);
-            invoiceRepository.save(byStatus);
+            int i = invoiceRepository.updateCountOfSmsByDealId(byStatus.getDealId());
+            if(i == 1) {
+                log.info("Update invoice and set count of sms = 1");
+            }else {
+                log.info("Update was rejected");
+            }
         }
         log.info("SCHEDULER WAS FINISH");
     }

7635e7c72e7ac79188c85a2f8f6af31af6ab9ab5
Dmitriy Pospelov <dmitriypospelov93@gmail.com>
2024-11-24 21:23:40 +0700

    Update bron info link

--- src/main/java/effective_mobile/com/utils/enums/CityInfo.java
+++ src/main/java/effective_mobile/com/utils/enums/CityInfo.java
@@ -7,23 +7,23 @@ public enum CityInfo {
 
 
     CHEL("ЧЕЛЯБИНСК", "https://chelyabinsk.exkursiacaramel.ru", 101, "C3:EXECUTING",
-            "https://карамельнаяфабрика.рф/chel"),
+            "https://карамельнаяфабрика.рф/bron"),
     TYUMEN("ТЮМЕНЬ", "https://tyumen.exkursiacaramel.ru", 113, "C9:EXECUTING",
-            "https://карамельнаяфабрика.рф/tumen"),
+            "https://карамельнаяфабрика.рф/bron"),
     MSKM("МОСКВА", "https://msk.exkursiacaramel.ru", 97, "FINAL_INVOICE",
-            "https://карамельнаяфабрика.рф"),
+            "https://карамельнаяфабрика.рф/bron"),
     MSKO("МОСКВА КРАСНОГОРСК", "https://exkursiacaramel.ru", 203, "C23:EXECUTING",
-            "https://карамельнаяфабрика.рф/msk_odin"),
+            "https://карамельнаяфабрика.рф/bron"),
     SAMARA("САМАРА", "https://samara.exkursiacaramel.ru", 99, "C1:EXECUTING",
-            "https://карамельнаяфабрика.рф/samara"),
+            "https://карамельнаяфабрика.рф/bron_sam"),
     KSR("КРАСНОЯРСК", "https://ksr.exkursiacaramel.ru", 109, "C13:EXECUTING",
-            "https://карамельнаяфабрика.рф/kras"),
+            "https://карамельнаяфабрика.рф/bron"),
     YAR("ЯРОСЛАВЛЬ", "https://yar.exkursiacaramel.ru", 103, "C5:EXECUTING",
-            "https://карамельнаяфабрика.рф/yarosl"),
+            "https://карамельнаяфабрика.рф/bron"),
     KZN("КАЗАНЬ", "https://kzn.exkursiacaramel.ru", 105, "C7:EXECUTING",
-            "https://карамельнаяфабрика.рф/kazan"),
+            "https://карамельнаяфабрика.рф/bron_kaz"),
     SPB("CАНКТ-ПЕТЕРБУРГ", "https://spb.exkursiacaramel.ru", 201, "C25:EXECUTING",
-            "https://карамельнаяфабрика.рф/spb");
+            "https://карамельнаяфабрика.рф/bron");
 
 
     final String name;
